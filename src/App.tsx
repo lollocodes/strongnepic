9bdf465a0b80c420a7d3025662902f26483899ab
lollocodes <louise_johansson.94@hotmail.com>
2023-09-13 19:51:21 +0200

    fetches user data from json file

--- src/App.tsx
+++ src/App.tsx
@@ -16,84 +16,23 @@ function App() {
   const [classes, setClasses] = useState<GymClass[]>([]);
   const [users, setUsers] = useState<User[]>([]);
 
-  
-  const mockUsers: User[] = [
-    {
-      id: 1,
-      email: "anna@epost.com",
-      password: "123",
-      role: "USER",
-      bookedClasses: []
-    },
-    {
-      id: 2,
-      email: "kalle@epost.com",
-      password: "123",
-      role: "USER",
-      bookedClasses: [1, 2]
-    },
-    {
-      id: 3,
-      email: "admin",
-      password: "123",
-      role: "ADMIN",
-      bookedClasses: []
-    }
-  ];
-
-  const mockClasses: GymClass[] = [
-    {
-      id: 1,
-      name: "Kettlebell",
-      capacity: 20,
-      booked: 5,
-      date: "2023-09-10",
-      starttime: "18:00",
-      endtime: "19:00",
-      bookedUsers: [2]
-    },
-    {
-      id: 2,
-      name: "Padel",
-      capacity: 15,
-      booked: 10,
-      date: "2023-09-12",
-      starttime: "18:00",
-      endtime: "19:00",
-      bookedUsers: [2]
-    },
-    {
-      id: 3,
-      name: "FYS",
-      capacity: 10,
-      booked: 8,
-      date: "2023-09-15",
-      starttime: "18:00",
-      endtime: "19:00",
-      bookedUsers: []
-    }
-  ];
-  
   useEffect(() => {
-    // fetch('/mockData.json')
-    //   .then((response) => {
-    //     if (!response.ok) {
-    //       throw new Error('Network response was not ok');
-    //     }
-    //     return response.json();
-    //   })
-    //   .then((data) => {
-    //     if (data.classes && data.users) {
-    //       setClasses(data.classes);
-    //       setUsers(data.users);
-    //     }
-    //   })
-    //   .catch((error) => {
-    //     console.error('Error fetching data:', error);
-    //   });
-      // You can use the mock data here if needed.
-      setUsers(mockUsers);
-      setClasses(mockClasses);
+    fetch('https://raw.githubusercontent.com/lollocodes/mockupServer/main/mockData.json')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (data.classes && data.users) {
+          setClasses(data.classes);
+          setUsers(data.users);
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
   }, []);
   
 
